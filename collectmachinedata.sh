cb174a8969079a624c546ed5ac9a22c93dc06b74
SharmajiKabetaDevesh <124495349+SharmajiKabetaDevesh@users.noreply.github.com>
2024-06-04 15:29:53 +0530

    Create collectmachinedata.sh

---
+++ collectmachinedata.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+
+
+###################################
+# Author:Devesh
+# Date: 4:6:2024
+#This is script is for learning
+
+
+set -e
+
+set -x
+
+set -o pipefail
+
+df -h
+
+free
+
+nproc
